7fb5e3567677ecaee88a8de8e5033fb9cd2327c2
Tom Searle <tsearle88@gmail.com>
2018-10-26 15:14:53 +0100

    add some more params to setup.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name="icd9cms",
     description='A utility for searching across the full icd9cms (2015) diagnosis hierarchy',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomolopolis/icd-util",
     version='0.1',
     author_email='thomas.searle@kcl.ac.uk',
     author='Tom Searle',
@@ -12,5 +19,10 @@ setup(
         'bs4',
         'requests',
     ],
-    include_package_data=True
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
